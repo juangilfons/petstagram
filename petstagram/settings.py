72ff8a5912f32c78c7c3c1a47376ce49a2281d0e
Juan Gil <juangilfons@gmail.com>
2024-10-18 03:05:46 -0300

    Changed database to PostgreSQL

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -72,17 +72,19 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'petstagram.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram',
+        'USER': 'juangil',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
